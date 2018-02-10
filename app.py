dcbbb5f3905ea768e2b7f71e517143d899fd4c3f
Shy Alon <shy.alon@gmail.com>
2018-02-10 09:49:31 +0200

    Requirements

--- app.py
+++ app.py
@@ -13,5 +13,18 @@ def homepage():
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
 
+
+@app.route('/download')
+def download():
+    the_time = datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <h2>It is currently {time}.</h2>
+    <h3>I am downloading data</h3>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
